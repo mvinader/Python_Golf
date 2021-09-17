7f92cddf9032843f4472d31423ba10fb705e2f29
mvinader <m.vinader@alumnos.upm.es>
2021-09-17 10:13:26 +0200

    Gráficos
    Imágenes de gráficos añadidas a Streamlit

--- Golf_chart.py
+++ Golf_chart.py
@@ -19,10 +19,17 @@ datos.describe()
 datos.head()
 
 datos[["EV", "AVG", "DR", "AVGDR", "ACC", "PUTT/H", "BIRD/R", "H/EAG"]].plot(kind="bar", subplots=True, title="Estadísticas para Tiger Woods")
-grafico=datos[["EV", "AVG", "DR", "AVGDR", "ACC", "PUTT/H", "BIRD/R", "H/EAG"]].plot(kind="bar", title="Estadísticas para Tiger Woods")
+plt.savefig("Grafica1.png")
+datos[["EV", "AVG", "DR", "AVGDR", "ACC", "PUTT/H", "BIRD/R", "H/EAG"]].plot(kind="bar", title="Estadísticas para Tiger Woods")
+plt.savefig("Grafica2.png")
 plt.show()
-#plt.savefig(fname=r"C:\Users\maviv", format='png')
 
 st.dataframe(datos.iloc[:,[1,2,3,4,5,6,7,8,9]])
+
+from PIL import Image
+image1 = Image.open("Grafica1.png")
+image2 = Image.open("Grafica2.png")
+st.image(image1, caption="Fuente: https://www.foxsports.com/golf/tiger-woods-player-stats?category=shots&groupId=1")
+st.image(image2, caption="Fuente: https://www.foxsports.com/golf/tiger-woods-player-stats?category=shots&groupId=1")
 #st.bar_chart(datos)
 #st.pyplot(datos)
